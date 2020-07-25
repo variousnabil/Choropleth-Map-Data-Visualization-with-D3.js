c60e907ecda176c39ef43e49b9871ff4c2d9dea6
variousnabil <varionabilr@gmail.com>
2020-07-25 21:24:01 +0700

    the choropleth is emerging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const urlEducationData = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
 const urlCountyData = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
 
+
 const w = 1000;
 const h = 600;
 
@@ -13,8 +14,7 @@ const margin = {
 
 const svg = d3.select('.container')
     .append('svg')
-    .attr('width', w)
-    .attr('height', h);
+    .attr('viewBox', [0, 0, w, h]);
 
 svg.append('text')
     .attr('x', w / 2)
@@ -32,3 +32,42 @@ svg.append('text')
     .attr('id', 'description')
     .style('font-size', '1em')
     .text(`Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)`);
+
+const getEducationData = axios.get(urlEducationData);
+const getCountyData = axios.get(urlCountyData);
+
+Promise.all([getEducationData, getCountyData])
+    .then(results => {
+        const educationData = results[0].data;
+        const countyData = results[1].data;
+        console.log('educationData', educationData);
+        console.log('countyData', countyData);
+
+        // education data simplified only 'id' and 'bachelorsOrHigher' data.
+        const data = {};
+        educationData.forEach(item => {
+            data[item.fips] = item['bachelorsOrHigher'];
+        });
+        console.log(data);
+
+        const colorScale = d3.scaleQuantize([3, 75], d3.schemeGreens[8]);
+        const path = d3.geoPath();
+
+        // county path
+        svg.append('g')
+            .selectAll('path')
+            .data(topojson.feature(countyData, countyData.objects.counties).features)
+            .join('path')
+            .attr('class', 'county')
+            .attr('fill', d => colorScale(data[d.id]))
+            .attr('d', path);
+
+        // state path
+        svg.append('path')
+            .datum(topojson.mesh(countyData, countyData.objects.states, (a, b) => a !== b))
+            .attr('fill', 'none')
+            .attr('stroke', 'white')
+            .attr('stroke-linejoin', 'round')
+            .attr('d', path);
+
+    });
